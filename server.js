15d40b2c2444baa97d1c65ebe30258e0f5b658e5
pradyunramesh <pradyun.ramesh@gmail.com>
2023-02-10 18:41:29 -0500

    Changed const initialization

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ var input = mini(process.argv.slice(2));
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port;
-port = input.port || 3000;
+const port = input.port || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
